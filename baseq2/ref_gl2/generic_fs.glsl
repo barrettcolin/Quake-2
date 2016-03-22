4c0c6687b57b295027fdc9cca2b6514c9e5815be
Colin Barrett <barrettcolin+github@gmail.com>
2016-03-22 22:13:36 +0000

    ALPHA_TEST_66 variant of generic_fs.glsl

--- baseq2/ref_gl2/generic_fs.glsl
+++ baseq2/ref_gl2/generic_fs.glsl
@@ -4,10 +4,12 @@ void main()
 {
     vec4 color = texture2D(diffuseTex, gl_TexCoord[0].st);
 
+#if defined (ALPHA_TEST_66)
     if(color.a <= 0.666f)
     {
         discard;
     }
+#endif
 
     gl_FragColor = color;
 }
