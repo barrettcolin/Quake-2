d446bd8f9a1927354d43de45a5f94086ed87eba3
Colin Barrett <barrettcolin@gmail.com>
2017-03-26 10:35:39 +0100

    More KHR_debug fixes

--- ref_gles2/gl_local.h
+++ ref_gles2/gl_local.h
@@ -82,6 +82,7 @@ extern void (GL_APIENTRY *qglViewport)(GLint x, GLint y, GLsizei width, GLsizei 
 
 #ifndef GL_KHR_debug
 typedef void (GL_APIENTRYP GLDEBUGPROCKHR)(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam);
+#define GL_DEBUG_OUTPUT_SYNCHRONOUS_KHR                         0x8242
 #endif
 extern void (GL_APIENTRY *qglDebugMessageCallbackKHR)(GLDEBUGPROCKHR callback, const void* userParam);
 
